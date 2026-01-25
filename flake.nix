f15cd9b53b4174898b8a63cdcc5200b6bf07e7c0
Scar Vorrix <scarlemmhd@gmail.com>
2026-01-25 01:21:42 -0600

    initial commit

---
+++ flake.nix
@@ -0,0 +1,89 @@
+{
+    description = "Edrakon – OpenXR → SteamLink OSC bridge";
+
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    outputs =
+        { self, nixpkgs }:
+        let
+            systems = [ "x86_64-linux" ];
+            forAll =
+                f:
+                builtins.listToAttrs (
+                    map (system: {
+                        name = system;
+                        value = f system;
+                    }) systems
+                );
+        in
+        {
+            packages = forAll (
+                system:
+                let
+                    pkgs = import nixpkgs { inherit system; };
+                in
+                {
+                    default = pkgs.callPackage ./package.nix { };
+                }
+            );
+
+            devShells = forAll (
+                system:
+                let
+                    pkgs = import nixpkgs { inherit system; };
+
+                    dotnetSdk = pkgs.dotnetCorePackages.sdk_9_0;
+
+                    libPath = pkgs.lib.makeLibraryPath [
+                        pkgs.openxr-loader
+                        pkgs.libglvnd
+                        pkgs.vulkan-loader
+                        pkgs.xorg.libX11
+                        pkgs.xorg.libXext
+                        pkgs.xorg.libXfixes
+                        pkgs.xorg.libXrandr
+                        pkgs.xorg.libXcursor
+                        pkgs.xorg.libXinerama
+                        pkgs.libdrm
+                        pkgs.udev
+                        pkgs.dbus
+                        pkgs.libuuid
+                    ];
+                in
+                {
+                    default = pkgs.mkShell {
+                        buildInputs = [
+                            dotnetSdk
+                            pkgs.nuget-to-json
+                            pkgs.openxr-loader
+                            pkgs.monado
+                            pkgs.icu
+                            pkgs.zlib
+                            pkgs.libkrb5
+                            pkgs.pkg-config
+                        ];
+
+                        DOTNET_CLI_TELEMETRY_OPTOUT = "1";
+
+                        shellHook = ''
+                            export LD_LIBRARY_PATH="${libPath}:$LD_LIBRARY_PATH"
+                            if [ -d /run/opengl-driver/lib ]; then
+                              export LD_LIBRARY_PATH="/run/opengl-driver/lib:$LD_LIBRARY_PATH"
+                            fi
+                            if [ -d /run/opengl-driver-32/lib ]; then
+                              export LD_LIBRARY_PATH="/run/opengl-driver-32/lib:$LD_LIBRARY_PATH"
+                            fi
+                            if [ -z "$XR_RUNTIME_JSON" ]; then
+                              ACTIVE_RUNTIME="$HOME/.config/openxr/1/active_runtime.json"
+                              if [ -f "$ACTIVE_RUNTIME" ]; then
+                                export XR_RUNTIME_JSON="$ACTIVE_RUNTIME"
+                              else
+                                export XR_RUNTIME_JSON=${pkgs.monado}/share/openxr/1/openxr_monado.json
+                              fi
+                            fi
+                        '';
+                    };
+                }
+            );
+        };
+}
---
+++ package.nix
@@ -0,0 +1,103 @@
+{
+    lib,
+    fetchFromGitHub,
+    buildDotnetModule,
+    dotnetCorePackages,
+    makeWrapper,
+    # Runtime deps
+    openxr-loader,
+    libglvnd,
+    vulkan-loader,
+    xorg,
+    libdrm,
+    udev,
+    dbus,
+    libuuid,
+    pkgs,
+}:
+
+let
+    repoOwner = "BlueCyro";
+
+    koboldSrc = fetchFromGitHub {
+        owner = repoOwner;
+        repo = "KoboldOSC";
+        rev = "08de424420c7b52751cb9e8dfddffdbdeb1699c8";
+        hash = "sha256-TRatrXgxSXiZZjV+7zR9BUXHjdV6+IrfQ+pQY36XUac=";
+    };
+in
+buildDotnetModule rec {
+    pname = "edrakon";
+    version = "0.1.0";
+
+    src = fetchFromGitHub {
+        owner = repoOwner;
+        repo = "Edrakon";
+        rev = "16f6488b333f36f2aa478ebda6833efac34732b3";
+        hash = "sha256-LoadLvhfxnoVfFO+Utuyhzs+Xcbl3fONyr7VoROEZkY=";
+    };
+
+    postUnpack = ''
+        cp -r ${koboldSrc} $sourceRoot/../KoboldOSC
+        chmod -R u+w $sourceRoot/../KoboldOSC
+    '';
+
+    patches = [
+        ./env-port.patch
+    ];
+
+    projectFile = "Edrakon.csproj";
+    nugetDeps = ./deps.json;
+
+    dotnet-sdk = dotnetCorePackages.sdk_9_0;
+    dotnet-runtime = dotnetCorePackages.runtime_9_0;
+
+    nativeBuildInputs = [ makeWrapper ];
+
+    runtimeDeps = [
+        openxr-loader
+        libglvnd
+        vulkan-loader
+        xorg.libX11
+        xorg.libXext
+        xorg.libXfixes
+        xorg.libXrandr
+        xorg.libXcursor
+        xorg.libXinerama
+        libdrm
+        udev
+        dbus
+        libuuid
+    ];
+
+    # TODO: replace with a arg helper to handle XR runtime.
+    postFixup = ''
+        wrapProgram $out/bin/Edrakon \
+          --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath runtimeDeps}" \
+          --prefix LD_LIBRARY_PATH : "/run/opengl-driver/lib" \
+          --set-default EDRAKON_PORT "9015" \
+          --run '
+              if [ -z "$XR_RUNTIME_JSON" ]; then
+                  ACTIVE_RUNTIME="$HOME/.config/openxr/1/active_runtime.json"
+                  if [ -f "$ACTIVE_RUNTIME" ]; then
+                      export XR_RUNTIME_JSON="$ACTIVE_RUNTIME"
+                  else
+                      ACTIVE_RUNTIME="/etc/xdg/openxr/1/active_runtime.json"
+                      if [ -f "$ACTIVE_RUNTIME" ]; then
+                          export XR_RUNTIME_JSON="$ACTIVE_RUNTIME"
+                      else
+                          export XR_RUNTIME_JSON="${pkgs.wivrn}/share/openxr/1/openxr_monado.json"
+                      fi
+                  fi
+              fi
+          '
+    '';
+
+    meta = {
+        description = "OpenXR to SteamLink OSC bridge";
+        homepage = "https://github.com/OWNER/Edrakon";
+        license = lib.licenses.mit;
+        mainProgram = "Edrakon";
+        platforms = [ "x86_64-linux" ];
+    };
+}
